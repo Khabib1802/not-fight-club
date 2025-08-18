fed9830f86061eee2464307edb4dae2d79d05aae
Khabib Gadzhimagomedov <habib31.03.2002@gmail.com>
2025-08-18 22:27:39 +0300

    feat: add menu and character section

--- script.js
+++ script.js
@@ -1,17 +1,73 @@
 'use strict'
 
-const formButton = document.querySelector('.form__button')
-const form = document.querySelector('.form-wrapper')
-
+const formWrapper = document.querySelector('.js-form-wrapper')
+const homePage = document.querySelector('.js-home-page')
+const formButton = document.querySelector('.js-form-btn')
+const homePageButton = document.querySelector('.js-form-btn')
+const characterUserName = document.querySelector('.character__user-name')
+const menuCharacterButton = document.querySelector('#characterid')
+const menuHomePageButton = document.querySelector('#homepageid')
+const character = document.querySelector('.character')
+const menu = document.querySelector('.menu')
 
 formButton.addEventListener('click', () => {
-  const userName = document.querySelector('.form__input').value
-  form.classList.toggle('none')
-  alert(userName)
+  const userName = document.querySelector('.js-form-input').value
+  formWrapper.classList.toggle('none')
+  homePage.classList.toggle('none')
+  menu.classList.toggle('none')
+  characterUserName.textContent = userName
 })
 
-document.querySelector('form').addEventListener('submit', (event) => event.preventDefault())
+menuCharacterButton.addEventListener('click', () => {
+  character.classList.remove('none')
+  homePage.classList.add('none')
+})
 
-const formContent = document.querySelector('.form')
+menuHomePageButton.addEventListener('click', () => {
+  character.classList.add('none')
+  homePage.classList.remove('none')
+})
 
-console.log(formContent.textContent)
+const popup = document.querySelector('.popup');
+const popupBtn = document.querySelector('.popup__btn');
+
+const togglePopup = (popup) => {
+  if (popup.classList.contains('hidden')) {
+    popup.classList.remove('hidden');
+  }
+};
+
+const handleButton = (event) => {
+  const element = event.target;
+
+  togglePopup(popup);
+}
+
+const popupClose = () => {
+  popup.classList.add('hidden');
+};
+
+popupBtn.addEventListener('click', popupClose);
+popup.addEventListener('click', (event) => {
+  if (event.target.classList.contains('popup')) {
+    popup.classList.add('hidden');
+  }
+});
+
+const changeImageButton = document.querySelector('.character__change-img-btn')
+
+changeImageButton.addEventListener('click', () => {
+  popup.classList.toggle('hidden')
+
+})
+
+const popupItems = document.querySelectorAll('.popup__list-item')
+
+popupItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    document.querySelector('.character__avatar img').src =
+      `assets/img/characters/${index + 1}.jpg`
+
+    popup.classList.toggle('hidden')
+  })
+})
